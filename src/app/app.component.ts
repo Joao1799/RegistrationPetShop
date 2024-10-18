c18373602fe710f79a59c0500f05713081527987
joao1799 <joaovictoralves552@gmail.com>
2024-10-18 06:40:43 -0300

    feature/1002

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component";
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,11 +4,13 @@ import { TableModule } from 'primeng/table';
 import { homeServices } from './home.service';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TableModule,CommonModule,RippleModule,ButtonModule],
+  imports: [TableModule,CommonModule,RippleModule,ButtonModule,MenubarModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   providers: [homeServices],
@@ -16,20 +18,65 @@ import { RippleModule } from 'primeng/ripple';
 export class HomeComponent {
   users: any;
   pets: any;
+  items:  MenuItem[] | undefined;
 
   constructor(private homeServices: homeServices){};
 
   ngOnInit() {
     this.getUsers(); 
+    this.menuBar();
   }
 
   getUsers(){
     this.homeServices.getUsers().subscribe((users)=> {
       this.users = users;
       console.log(this.users)
-    // this.pets = this.users.map((user: any) => user.pets);
-    // console.log(this.pets)
     })
+  }
+
+  menuBar(){
+    this.items = [
+      {
+          label: 'Home',
+          icon: 'fa fa-user'
+      },
+      {
+          label: 'Atendimentos',
+          icon: 'pi pi-calendar',
+          items: [
+              {
+                  label: 'Consulta',
+                  icon: 'fa fa-notes-medical'
+              },
+              {
+                  label: 'Vacinação',
+                  icon: 'fa fa-syringe'
+              },
+              {
+                  label: 'Retorno',
+                  icon: 'fa fa-rotate-left'
+              },
+          ]
+      },
+      {
+        label: 'Registros',
+        icon: 'pi pi-align-justify',
+        items: [
+            {
+                label: 'Usuario',
+                icon: 'pi pi-user-plus'
+            },
+            {
+                label: 'Pet',
+                icon: 'fa fa-dog'
+            },
+            {
+                label: 'Retorno',
+                icon: 'fa fa-calendar-days'
+            }
+        ]
+      }
+  ]
   }
 
 }
