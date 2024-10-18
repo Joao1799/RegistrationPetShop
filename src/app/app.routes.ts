61ce723b022890a9f8e381e34b40c68e9b092b05
joao victor <joao.jvja@pf.gov.br>
2024-10-18 14:56:22 -0300

    feature/1002 BugFixs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterUserComponent } from './components/register-user/register-user.component';
+import { RegisterPetComponent } from './components/register-pet/register-pet.component';
+import { RegisterServiceComponent } from './components/register-service/register-service.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },  // Rota para o HomeComponent
+  { path: 'registrarUsuario', component: RegisterUserComponent }, // Rota para RegistrarUser
+  { path: 'registrarPet', component: RegisterPetComponent },       // Rota para RegistrarPet
+  { path: 'registrarServico', component: RegisterServiceComponent }, // Rota para RegistrarService
+];
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,6 +6,7 @@ import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
 import { MenuItem } from 'primeng/api';
 import { MenubarModule } from 'primeng/menubar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -20,16 +21,104 @@ export class HomeComponent {
   pets: any;
   items:  MenuItem[] | undefined;
 
-  constructor(private homeServices: homeServices){};
+  constructor(private homeServices: homeServices,private router: Router){};
 
   ngOnInit() {
     this.getUsers(); 
     this.menuBar();
+
+    this.users = [
+      {
+        ownerName:"joao",
+        email:"joaovictor@gmail.com",
+        atendimentos:true,
+        id:"123",
+      },
+      {
+        ownerName:"pedro",
+        email:"jpedrovictor@gmail.com",
+        atendimentos:true,
+        id:"1534",
+      },
+      {
+        ownerName:"carlos",
+        email:"123r@gmail.com",
+        atendimentos:true,
+        id:"1321",
+      },      {
+        ownerName:"jota",
+        email:"jota@gmail.com",
+        atendimentos:true,
+        id:"1234",
+      },
+      {
+        ownerName:"joao",
+        email:"joaovictor@gmail.com",
+        atendimentos:true,
+        id:"1666",
+      },      {
+        ownerName:"joao",
+        email:"joaovictor@gmail.com",
+        atendimentos:true,
+        id:"123",
+      },
+      {
+        ownerName:"pedro",
+        email:"jpedrovictor@gmail.com",
+        atendimentos:true,
+        id:"1534",
+      },
+      {
+        ownerName:"carlos",
+        email:"123r@gmail.com",
+        atendimentos:true,
+        id:"1321",
+      },      {
+        ownerName:"jota",
+        email:"jota@gmail.com",
+        atendimentos:true,
+        id:"1234",
+      },
+      {
+        ownerName:"joao",
+        email:"joaovictor@gmail.com",
+        atendimentos:true,
+        id:"1666",
+      },      {
+        ownerName:"joao",
+        email:"joaovictor@gmail.com",
+        atendimentos:true,
+        id:"123",
+      },
+      {
+        ownerName:"pedro",
+        email:"jpedrovictor@gmail.com",
+        atendimentos:true,
+        id:"1534",
+      },
+      {
+        ownerName:"carlos",
+        email:"123r@gmail.com",
+        atendimentos:true,
+        id:"1321",
+      },      {
+        ownerName:"jota",
+        email:"jota@gmail.com",
+        atendimentos:true,
+        id:"1234",
+      },
+      {
+        ownerName:"joao",
+        email:"joaovictor@gmail.com",
+        atendimentos:true,
+        id:"1666",
+      }
+    ]
   }
 
   getUsers(){
     this.homeServices.getUsers().subscribe((users)=> {
-      this.users = users;
+      // this.users = users;
       console.log(this.users)
     })
   }
@@ -38,7 +127,8 @@ export class HomeComponent {
     this.items = [
       {
           label: 'Home',
-          icon: 'fa fa-user'
+          icon: 'fa fa-user',
+          routerLink: '/home'
       },
       {
           label: 'Atendimentos',
@@ -46,7 +136,7 @@ export class HomeComponent {
           items: [
               {
                   label: 'Consulta',
-                  icon: 'fa fa-notes-medical'
+                  icon: 'fa fa-notes-medical',
               },
               {
                   label: 'Vacinação',
@@ -62,21 +152,25 @@ export class HomeComponent {
         label: 'Registros',
         icon: 'pi pi-align-justify',
         items: [
-            {
-                label: 'Usuario',
-                icon: 'pi pi-user-plus'
-            },
-            {
-                label: 'Pet',
-                icon: 'fa fa-dog'
-            },
-            {
-                label: 'Retorno',
-                icon: 'fa fa-calendar-days'
-            }
+          {
+            label: 'Usuario',
+            icon: 'pi pi-user-plus',
+            routerLink: '/registrarUsuario',
+          },
+          {
+            label: 'Pet',
+            icon: 'fa fa-dog',
+            routerLink: '/registrarPet',
+          },
+          {
+            label: 'Serviço',
+            icon: 'fa fa-calendar-days',
+            routerLink: '/registrarServico',
+          },
         ]
       }
   ]
   }
 
 }
+
